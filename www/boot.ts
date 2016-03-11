e84bb232e24417adb6156250e55acd46bf399ef1
Jordan Last <jordan.michael.last@gmail.com>
2016-03-10 19:01:31 -0700

    Getting rid of github pages specific stuff

--- www/boot.ts
+++ www/boot.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './components/app/app.component.ts';
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import {provide} from 'angular2/core';
 import {createStore} from 'redux';
 import {rootReducer} from './redux/reducers.ts';
@@ -8,4 +8,4 @@ import {Constants} from './services/constants.service.ts';
 
 const store = createStore(rootReducer);
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy }), provide(Constants.REDUX_STORE, {useValue: store})]);
+bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(Constants.REDUX_STORE, {useValue: store})]);
