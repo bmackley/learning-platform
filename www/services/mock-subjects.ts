0fac547f08c47b150c84d9137d4324d190a33d95
tarahkerr <tarahkerr@gmail.com>
2016-04-04 17:27:23 -0600

    Added photos to subjects and homepage

--- www/services/mock-subjects.ts
+++ www/services/mock-subjects.ts
@@ -3,13 +3,13 @@ import {Subject} from '../components/subject/subject.ts';
 // var app = angular.module("sampleApp", ["firebase"]);
 export var Subjects: Subject[] = [
     {"id": 11, "name": "Math", "pictureFilePath" :  "../components/subject/backgroundPhotos/math.jpg"},
-    {"id": 12, "name": "Science", "pictureFilePath" :  ""},
-    {"id": 13, "name": "English", "pictureFilePath" :  ""},
-    {"id": 14, "name": "Psychology", "pictureFilePath" :  ""},
-    {"id": 15, "name": "Theater", "pictureFilePath" :  ""},
-    {"id": 16, "name": "Music", "pictureFilePath" :  ""},
-    {"id": 17, "name": "Technology", "pictureFilePath" :  ""},
-    {"id": 18, "name": "Business", "pictureFilePath" :  ""},
-    {"id": 19, "name": "Advertising", "pictureFilePath" :  ""},
-    {"id": 20, "name": "Social Science", "pictureFilePath" :  ""}
+    {"id": 12, "name": "Science", "pictureFilePath" :  "../components/subject/backgroundPhotos/science.jpg"},
+    {"id": 13, "name": "English", "pictureFilePath" :  "../components/subject/backgroundPhotos/english.jpg"},
+    {"id": 14, "name": "Psychology", "pictureFilePath" :  "../components/subject/backgroundPhotos/psychology.jpg"},
+    {"id": 15, "name": "Theater", "pictureFilePath" :  "../components/subject/backgroundPhotos/theater.jpg"},
+    {"id": 16, "name": "Music", "pictureFilePath" :  "../components/subject/backgroundPhotos/music.jpg"},
+    {"id": 17, "name": "Technology", "pictureFilePath" :  "../components/subject/backgroundPhotos/technology.jpg"},
+    {"id": 18, "name": "Business", "pictureFilePath" :  "../components/subject/backgroundPhotos/business.jpg"},
+    {"id": 19, "name": "Advertising", "pictureFilePath" :  "../components/subject/backgroundPhotos/advertising.jpg"},
+    {"id": 20, "name": "Social Science", "pictureFilePath" :  "../components/subject/backgroundPhotos/socialScience.jpg"}
 ];
